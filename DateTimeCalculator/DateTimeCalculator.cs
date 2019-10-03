fa2f8188d9219fc3ba4ad6e75e1ca55a3c4cedd3
kapitan21099 <kapitan21099@gmail.com>
2019-10-03 20:01:00 +0300

    add project

---
+++ DateTimeCalculator/DateTimeCalculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeCalculator
+{
+    class DateTimeCalc
+    {
+        public static int Dil(int a, int b)
+        {
+            if (b == 0)
+            {
+                return 0;
+            }
+            return a / b;
+        }
+        public static DateTime PlusDay(DateTime data, int k)
+        {
+
+            new DateTime(data.Year, data.Month, data.Day + k);
+
+            return new DateTime(data.Year, data.Month, data.Day + k);
+        }
+        public static DateTime PlusYear(DateTime data, int k)
+        {
+            return new DateTime(data.Year + k, data.Month, data.Day);
+        }
+        public static DateTime PlusMonth(DateTime data, int k)
+        {
+            int dayInMonth=DateTime.DaysInMonth(data.Year, data.Month);
+            bool dayInYuear = DateTime.IsLeapYear(data.Year);
+
+            return new DateTime(data.Year, data.Month + k, data.Day);
+        }
+        public static DateTime PlusHour(DateTime data, int k)
+        {
+            return new DateTime(data.Year, data.Month, data.Day, data.Hour + k, data.Minute, data.Second);
+
+        }
+        
+    }
+}
---
+++ DateTimeCalculator/DateTimeCalculatorTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeCalculator
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void DilTest1()
+        {
+            var res = DateTimeCalculator.DateTimeCalc.Dil(10, 5);
+            Assert.AreEqual(2, res);
+        }
+        [TestMethod]
+        public void DilTest2()
+        {
+            var res = DateTimeCalculator.DateTimeCalc.Dil(10, 0);
+            Assert.AreEqual(0, res);
+        }
+        [TestMethod]
+        public void PlusDayTest1()
+        {
+            DateTime data = new DateTime(2019, 10, 23);
+            var result = DateTimeCalc.PlusDay(data, 10);
+            DateTime data1 = new DateTime(2019, 10, 24);
+            Assert.AreEqual(data1, result);           
+        }
+
+        [TestMethod]
+        public void PlusYearTest1()
+        {
+            DateTime data = new DateTime(2019, 10, 3);
+            var result = DateTimeCalc.PlusYear(data, 6);
+            DateTime data1 = new DateTime(2025, 10, 3);
+            Assert.AreEqual(data1, result);
+        }
+        [TestMethod]
+        public void PlusMonthTest1()
+        {
+            DateTime data = new DateTime(2019, 5, 3);
+            var result = DateTimeCalc.PlusMonth(data, 6);
+            DateTime data1 = new DateTime(2019, 11, 3);
+            Assert.AreEqual(data1, result);
+        }
+
+
+    }
+}
