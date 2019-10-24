66c1ec09858e4835e8777e1d6c7e49b80c7f9ad7
kapitan21099 <kapitan21099@gmail.com>
2019-10-24 18:54:12 +0300

    Skobki Relize

--- Skobki_Relize/Program.cs
+++ Skobki_Relize/Program.cs
@@ -10,19 +10,24 @@ namespace Skobki_Relize
     {
         static void Main(string[] args)
         {
-            string str = "10+12+)(95-13)+10()";
-            char[] strChar = str.ToCharArray();
+            string str = "10+12+()(95-13)+10()";
             int a = 0;
-            foreach (char c in strChar)
+            for (int i = 0; i < str.Length; i++)
             {
+                char c = str[i];
                 if (c == ')')
                     a -= 1;
                 if (c == '(')
                     a += 1;
                 if (a < 0)
-                    Console.WriteLine("False");
-                Console.ReadLine();
+                {
+                    break;
+                }
             }
+            if (a==0)
+                Console.WriteLine("True");
+            else Console.WriteLine("False");
+            Console.ReadLine();
         }
     }
 }
