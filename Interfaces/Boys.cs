b4f5f93772fd1633724f1906adc5fbe8f584009a
kapitan21099 <kapitan21099@gmail.com>
2019-10-24 20:05:04 +0300

    Interfaces people

---
+++ Interfaces/Boys.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Boys:IStudent
+    {
+        public string Name { get; set;}
+        public Boys(string name)
+        {
+            Name = name;
+        }
+        public int Old { get; set; }
+        public Boys(int old)
+        {
+            Old = old;
+        }
+        public void GetInfo(int old)
+        {            
+            Console.WriteLine(old);
+        }
+
+        public string Info(string name)
+        {
+            return name;
+        }
+    }
+}
---
+++ Interfaces/Girls.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Girls:IStudent
+    {
+        public string Name { get;}
+        public Girls(string name)
+        {
+            Name = name;
+        }
+        public int Old { get; }
+        public Girls(int old)
+        {
+            Old = old;
+        }
+        public void GetInfo(int old)
+        {
+            Console.WriteLine(old);
+        }
+
+        public string Info(string name)
+        {
+            return name;
+        }
+    }
+}
+
