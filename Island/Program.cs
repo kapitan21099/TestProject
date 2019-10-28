96d4de769643f8b6e12c70b0608f2f23022dcf45
kapitan21099 <kapitan21099@gmail.com>
2019-10-28 22:57:13 +0200

    primitive code island of 1, but they work

--- Island/Program.cs
+++ Island/Program.cs
@@ -10,8 +10,11 @@ namespace Island
     {
         static void Main(string[] args)
         {
-            int row = 10;
-            int col = 10; //Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number of rows");
+            int row = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number of columns");
+            int col = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
             int[,] array = new int[row, col];
             Random rand = new Random();
             for (int i = 0; i < row; i++)
@@ -19,7 +22,10 @@ namespace Island
                 for (int j = 0; j < col; j++)
                 {
                     array[i, j] = rand.Next(0, 2);
-
+                    if (array[i, j] == 1)
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    else
+                        Console.ForegroundColor = ConsoleColor.Red;
                     Console.Write(array[i, j] + "  ");
                 }
                 Console.WriteLine();
@@ -33,10 +39,46 @@ namespace Island
                     {
                         result++;
                     }
-
                 }
             }
-            Console.WriteLine(result);
+            for (int i = 1; i < row - 1; i++)
+            {
+                if (array[i, 0] == 1 && array[i - 1, 0] == 0 && array[i + 1, 0] == 0 && array[i - 1, 1] == 0 && array[i, 1] == 0 && array[i + 1, 1] == 0)
+                {
+                    result++;
+                }
+            }
+            for (int i = 1; i < row - 1; i++)
+            {
+                if (array[i, col-1] == 1 && array[i - 1, col - 1] == 0 && array[i + 1, col - 1] == 0 && array[i - 1, col - 2] == 0 && array[i, col - 2] == 0 && array[i + 1, col - 2] == 0)
+                {
+                    result++;
+                }
+            }
+            for (int j = 1; j < col - 1; j++)
+            {
+                if (array[0, j] == 1 && array[0, j-1] == 0 && array[0, j+1] == 0 && array[1, j-1] == 0 && array[1, j] == 0 && array[1, j+1] == 0)
+                {
+                    result++;
+                }
+            }
+            for (int j = 1; j < col - 1; j++)
+            {
+                if (array[row-1, j] == 1 && array[row-1, j-1] == 0 && array[row-1, j+1] == 0 && array[row-2, j-1] == 0 && array[row-2, j] == 0 && array[row-2, j+1] == 0)
+                {
+                    result++;
+                }
+            }
+            if (array[0, 0] == 1 && array[1, 0] == 0 && array[0, 1] == 0 && array[1, 1] == 0)
+                result++;
+            if (array[row-1, 0] == 1 && array[row-2, 0] == 0 && array[row-2, 1] == 0 && array[row-1, 1] == 0)
+                result++;
+            if (array[0, col-1] == 1 && array[0, col-2] == 0 && array[1, col-2] == 0 && array[1, col-1] == 0)
+                result++;
+            if (array[row-1, col-1] == 1 && array[row-2, col-1] == 0 && array[row-1, col-2] == 0 && array[row-2, col-2] == 0)
+                result++;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\n Number island is " + result);
             Console.ReadLine();
         }
     }
