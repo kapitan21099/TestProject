2cc756a9e9dfed77bf79c403099d3382ad7477c5
kapitan21099 <kapitan21099@gmail.com>
2019-10-31 23:12:51 +0200

    polindrom

---
+++ Palindrom/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello. Enter your word");
+            string word = Console.ReadLine();
+            if (IsContainsOnlyLetters(word))
+            {
+                int timer = 0;
+                for (int i = 0; i <= (word.Length / 2 - 1); i++)
+                {
+                    if (word[i] == word[word.Length - 1 - i])
+                    {
+                        timer++;
+                    }
+                }
+                if (timer == word.Length / 2)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(" This word is polindrom");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("This word is not polindrom");
+                }
+            }
+            else if(IsOnlySpaces(word))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Your word has {0} spaces. Is polindrom",word.Length);
+            }
+            else
+            {
+                Console.WriteLine("Not all characters are letters.Please try again");
+            }
+            Console.ReadLine();
+        }
+        static bool IsContainsOnlyLetters(string word)
+        {
+            for (int i = 0; i < word.Length; ++i)
+            {
+                char elementOfWord = word[i];
+                if (!char.IsLetter(elementOfWord))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static bool IsOnlySpaces(string word)
+        {
+            for (int i = 0; i < word.Length; ++i)
+            {
+                char elementOfWord = word[i];              
+                if (elementOfWord!=' ')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
